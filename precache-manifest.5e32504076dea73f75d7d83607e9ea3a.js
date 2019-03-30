f4fe5fa125f770803efd549d38285eeafbc7b49f
assiri <assiri@assiris-MacBook-Pro.local>
2019-03-30 09:08:44 +0300

    Updates

---
+++ precache-manifest.5e32504076dea73f75d7d83607e9ea3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "587a73ee8bbf13960dd0",
+    "url": "/qaispwaapp/static/js/runtime~main.ee5eca7c.js"
+  },
+  {
+    "revision": "a0c0c324968023cb43da",
+    "url": "/qaispwaapp/static/js/main.10cffce4.chunk.js"
+  },
+  {
+    "revision": "a183812656716bafad26",
+    "url": "/qaispwaapp/static/js/2.1fe65178.chunk.js"
+  },
+  {
+    "revision": "a183812656716bafad26",
+    "url": "/qaispwaapp/static/css/2.14860cbc.chunk.css"
+  },
+  {
+    "revision": "a643c8bfb6cfc6c37902b524590e0367",
+    "url": "/qaispwaapp/index.html"
+  }
+];
