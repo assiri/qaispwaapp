d951248282900c41382d3c5851b8035992a250c7
assiri <assiri@hotmail.com>
2019-03-30 18:40:44 +0300

    Update precache-manifest.ff2d76a23b6cfc0ede1da7957fc5dbcd.js

--- precache-manifest.ff2d76a23b6cfc0ede1da7957fc5dbcd.js
+++ precache-manifest.ff2d76a23b6cfc0ede1da7957fc5dbcd.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = [
   {
     "revision": "0f90c194f72ffe248eaa",
-    "url": "/qaispwaapp/static/js/runtime~main.fef73312.js"
+    "url": "/static/js/runtime~main.fef73312.js"
   },
   {
     "revision": "306d6038d33589d446b0",
-    "url": "/qaispwaapp/static/js/main.fe96712c.chunk.js"
+    "url": "/static/js/main.fe96712c.chunk.js"
   },
   {
     "revision": "9bf7a8a1ed7a0e6b7e6d",
-    "url": "/qaispwaapp/static/js/2.e1f1c585.chunk.js"
+    "url": "/static/js/2.e1f1c585.chunk.js"
   },
   {
     "revision": "9bf7a8a1ed7a0e6b7e6d",
-    "url": "/qaispwaapp/static/css/2.640e4836.chunk.css"
+    "url": "/static/css/2.640e4836.chunk.css"
   },
   {
     "revision": "265018e6dda822633003773ba8a24e73",
-    "url": "/qaispwaapp/index.html"
+    "url": "/index.html"
   },
   {
-    "url": "/qaispwaapp/images/icons/icon-72x72.png"
+    "url": "/images/icons/icon-72x72.png"
   },
   {
   "url" : "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js"
   },
   {
-    "url":"/qaispwaapp/manifest.json"
+    "url":"/manifest.json"
   }
 ];
