adb24c69e857bfeecf64028a8b5e9c73b2de4f4c
assiri <assiri@hotmail.com>
2019-03-30 19:30:35 +0300

    Update precache-manifest.2dc49f123fecf712bdee1a1b761fbdd9.js

--- precache-manifest.2dc49f123fecf712bdee1a1b761fbdd9.js
+++ precache-manifest.2dc49f123fecf712bdee1a1b761fbdd9.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
     "revision": "0f90c194f72ffe248eaa",
-    "url": "/qaispwaapp/static/js/runtime~main.fef73312.js"
+    "url": "/static/js/runtime~main.fef73312.js"
   },
   {
     "revision": "306d6038d33589d446b0",
-    "url": "/qaispwaapp/static/js/main.fe96712c.chunk.js"
+    "url": "/static/js/main.fe96712c.chunk.js"
   },
   {
     "revision": "b8b5913f33538f315152",
-    "url": "/qaispwaapp/static/js/2.e1f1c585.chunk.js"
+    "url": "/static/js/2.e1f1c585.chunk.js"
   },
   {
     "revision": "b8b5913f33538f315152",
-    "url": "/qaispwaapp/static/css/2.7c880d99.chunk.css"
+    "url": "/static/css/2.7c880d99.chunk.css"
   },
   {
     "revision": "b332583fd7fa990fa88af119bed77e9d",
-    "url": "/qaispwaapp/index.html"
+    "url": "/index.html"
   }
 ];
